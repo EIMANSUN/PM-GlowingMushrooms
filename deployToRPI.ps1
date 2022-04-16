3dffbbd87b65a6f8c0921ae0dda686bbaa9f0a2b
EIMANSUN <Eimantas.Vengris@gmail.com>
2022-04-16 17:56:25 +0300

    Added env

---
+++ deployToRPI.ps1
@@ -0,0 +1,8 @@
+$sourceDir = "D:\Mano Projaktai\Pretty Much\PM-GlowingMushrums\source"
+$destDir = "/home/eimansun"
+foreach ($file in Get-Childitem -Path $sourceDir) {
+    scp $file.FullName eimansun@RPI-Z.local:$destDir
+}
+
+#Invoke-Command -HostName "eimansun@RPI-Z.local" -ScriptBlock { touch yo } -KeyFilePath /Eiman/UserAKey_rsa
+ssh eimansun@RPI-Z.local 'uname;hostname;date;touch yo && exit &'
